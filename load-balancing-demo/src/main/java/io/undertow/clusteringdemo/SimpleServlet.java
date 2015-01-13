f4e830fe60e375d72635b9160c5ace1121793f92
Stuart Douglas <stuart.w.douglas@gmail.com>
2015-01-13 12:44:46 +1100

    Fix up some other stuff

--- load-balancing-demo/src/main/java/io/undertow/clusteringdemo/SimpleServlet.java
+++ load-balancing-demo/src/main/java/io/undertow/clusteringdemo/SimpleServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 /**
@@ -15,6 +16,15 @@ public class SimpleServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().write("Server " + System.getProperty("server.name"));
+        resp.getWriter().write("Server " + System.getProperty("server.name") + " ");
+        if(req.getParameter("stateful") != null) {
+            HttpSession session = req.getSession(true);
+            Integer count = (Integer) session.getAttribute("count");
+            if(count == null) {
+                count = 1;
+            }
+            resp.getWriter().write("Request Count " + count);
+            session.setAttribute("count", ++count);
+        }
     }
 }
